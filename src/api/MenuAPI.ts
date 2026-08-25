bf9aa2c59e2fef1ebe9dc362349c803865cc153b
AdeNoah <adegboyenoah@gmail.com>
2026-08-25 08:04:13 +0100

    created the layout required for the menu

--- src/api/MenuAPI.ts
+++ src/api/MenuAPI.ts
@@ -1,5 +1,12 @@
 // import {type mealData} from "../data/types"
 
-// const alcoholicDrinkURL
-// const nonAlcoholicDrinkURL
-// const cocktailURL
+
+// drink api links 
+// const alcoholicDrinkURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic"
+// const nonAlcoholicDrinkURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic"
+// const cocktailURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail"
+
+// meals api links 
+// startersURL = "http://www.themealdb.com/api/json/v1/1/filter.php?a=Chinese"
+// mainCoursesURL = "http://www.themealdb.com/api/json/v1/1/filter.php?a=French"
+// dessertsURL = "http://www.themealdb.com/api/json/v1/1/filter.php?a=German"
--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,15 +1,38 @@
+// import { BrowserRouter } from 'react-router-dom';
+// Routes, Route, Link, Outlet, useParams
+
+import { Link } from "react-router-dom"
+
+
+
 
 const Menu = () => {
   return (
     <>
-      <div className="w-full py-4 flex justify-between ">
-        <div className="bg-cream">
-          <p>CATEGORIES</p>
-          
+      <div className="w-full py-4 flex justify-between gap-4">
+        <div className="bg-cream p-4 rounded-md shadow-lg">
+          <p className="font-primary font-bold text-lg border-b mb-4">CATEGORIES</p>
+          <ul className="mb-4">
+            <li className ="border-b mb2">
+              <p className="font-secondary ">Meals</p>
+            </li>
+            <li><Link to="/starters">Starters</Link></li>
+            <li><Link to="/main-courses">Main Course</Link></li>
+            <li><Link to="/desserts">Desserts</Link></li>
+          </ul>
+
+          <ul>
+            <li className ="border-b ">
+              <p className="font-secondary ">Drinks</p>
+            </li>
+            <li><Link to="/alcoholic">Alcoholic</Link></li>
+            <li><Link to="/non-alcoholic">Non-Alcoholic</Link></li>
+            <li><Link to="/cocktails">Cocktails</Link></li>
+          </ul>
         </div>
 
-        <div>
-          <p>CATEGORY</p>
+        <div className="bg-cream p-4 rounded-md shadow-lg fit-content flex-1">
+          <p className="font-primary font-bold text-lg border-b mb-4 text-center">CATEGORY</p>
         </div>
       </div>
     </>
